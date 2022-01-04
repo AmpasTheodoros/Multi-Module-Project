a06531c4793a1b097ee26f9293da874a737a55c7
Ampas Theodoros <atheodoro@csd.auth.gr>
2022-01-04 19:55:11 +0200

    person to html

--- web/src/main/java/com/example/controller/person/PersonController.java
+++ web/src/main/java/com/example/controller/person/PersonController.java
@@ -4,6 +4,7 @@ import com.example.model.person.Person;
 import com.example.service.person.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,7 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 public class PersonController {
 
     @Autowired
@@ -46,15 +47,18 @@ public class PersonController {
         return service.updatePerson(id,person);
     }
 
-    @GetMapping
-    String getPeople(Model model){
-        model.addAttribute("something", "controller");
-        model.addAttribute("people", Arrays.asList(
-                new Person(103,"nicolas","ff"),
-                new Person(104,"John","Wick"),
-                new Person(105,"Rdj","ew"),
-                new Person(106,"teo","ds")
-                ));
-        return "people.html";
+    @GetMapping("/people")
+    public String getPeople(Model model){
+//        model.addAttribute("something", "controller");
+//        model.addAttribute("people", Arrays.asList(
+//                new Person(103,"nicolas","ff"),
+//                new Person(104,"John","Wick"),
+//                new Person(105,"Rdj","ew"),
+//                new Person(106,"teo","ds")
+//                ));
+        List<Person> personList = service.getPersons();
+        model.addAttribute("people",personList);
+        System.out.println(personList);
+        return "people";
     }
 }
--- web/src/main/java/com/example/controller/person/SecurityConfig.java
+++ web/src/main/java/com/example/controller/person/SecurityConfig.java
@@ -5,12 +5,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.
                 authorizeRequests()
+                .antMatchers("/people")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
