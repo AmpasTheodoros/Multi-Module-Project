02aeea26b6dcdf304751dfd362f33bdc9a67105a
Fillip <iliadis.fil@gmail.com>
2022-01-06 20:46:02 +0200

    Person entity creation

--- domain/src/main/java/com/example/model/person/Person.java
+++ domain/src/main/java/com/example/model/person/Person.java
@@ -1,21 +1,90 @@
 package com.example.model.person;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Document
-@AllArgsConstructor
-@NoArgsConstructor
 @ToString
 public class Person {
     @Id
-    private int id;
-    private String name;
-    private String specialist;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String contactDetails;
+
+    private String comments;
+
+    public Person(String firstName, String lastName, String address, String contactDetails, String comments) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.contactDetails = contactDetails;
+        this.comments = comments;
+    }
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(String contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
 }
